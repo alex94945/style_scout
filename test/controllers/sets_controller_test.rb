edf442319d5121cddf53e455b071bd7722a87723
Alex Young <alex94945@gmail.com>
2016-03-26 17:31:17 -0700

    add hover effect
    also updated old filenames/refs which contained “Sets” which are now
    called “Appointments” due to reserved word.

--- test/controllers/sets_controller_test.rb
+++ test/controllers/sets_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class SetsControllerTest < ActionController::TestCase
+class AppointmentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
