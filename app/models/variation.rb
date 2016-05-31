77f86b947577efe7ad197adfcfe4d7bb031b8f23
Alex Young <alex94945@gmail.com>
2016-05-30 17:39:46 -0700

    temporarily fix error due to incomplete implementation of variations

--- app/models/variation.rb
+++ app/models/variation.rb
@@ -1,5 +1,5 @@
 class Variation < ActiveRecord::Base
 
-  belongs_to :style
+
 
 end
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "visiting the application", type: :feature, js: true do
 
-  it "it shows appointments index if logged in" do 
+  it "it shows appointments index if signed in" do 
     @user = User.create(email: "test@test.com", password: "123456789", password_confirmation: '123456789', name: 'Alex Young')
     login_as(@user, scope: :user)
     visit '/'
@@ -11,7 +11,7 @@ describe "visiting the application", type: :feature, js: true do
 
   it "it shows the login page if not logged in" do 
     visit'/'
-    expect(page).to have_css('h2', text: 'Log in')
+    expect(page).to have_css('h2', text: 'Sign in')
   end
 
 end
