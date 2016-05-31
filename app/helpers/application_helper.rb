a6c99ef83b8d171cf54936e84e7f0d1eca565af7
Alex Young <alex94945@gmail.com>
2016-05-30 20:24:48 -0700

    make datatables thumbnail bigger

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,3 @@
 module ApplicationHelper
 
-	# def style_name_helper(style) **DEPRECATED**
-		# name = ""
-		# if style.name.present?
-		# 	name = style.name
-		# elsif style.vendor_style_number.present?
-		# 	name = style.vendor_style_number
-		# else
-		# 	name = style.id
-		# end	
-		# return name
-
-		# style.name.present? ? style.name : style.vendor_style_number
-
-	# end
-
 end
