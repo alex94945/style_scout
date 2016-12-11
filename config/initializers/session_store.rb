418a958e7da70181960b86d8d837735f3dcced37
Alex Young <alex94945@gamil.com>
2016-12-10 21:29:30 -0800

    change repo name to style scout

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_merchandise_session'
+Rails.application.config.session_store :cookie_store, key: '_style_scout_session'
