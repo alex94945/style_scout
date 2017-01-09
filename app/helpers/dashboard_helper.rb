d02c0e171763ed6e678fdf2e4214f2037cc532ed
Alex Young <alex94945@gamil.com>
2017-01-08 20:25:12 -0800

    more work on dashboard merch month selector

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,19 +1,30 @@
 module DashboardHelper
 
-  def options_for_merch_date_picker(params)
+  def options_for_merch_month_picker(params)
     html = ""
     12.times do |i|
       month = i+1
       html += content_tag(:option, value: month, selected: current_or_active_merch_month(params, month)) do
-        month_range_to_month_name(month)
+        date_range_to_month_name(month)
       end
     end
     return html.html_safe
   end
 
-  def month_range_to_month_name(month)
-    start_date = MerchCalendar.start_of_month(Date.current.year,merch_month: month)
-    end_date = MerchCalendar.end_of_month(Date.current.year,merch_month: month)
+  def options_for_merch_year_picker(params)
+    html_year = ""
+    years = [2015, 2016, 2017, 2018, 2019, 2020]
+    years.each do |merch_year|
+      html_year += content_tag(:option, value: merch_year, selected: current_or_active_merch_year(params, merch_year)) do
+        merch_year.to_s
+      end
+    end
+    return html_year.html_safe
+  end
+
+  def date_range_to_month_name(month)
+    start_date = MerchCalendar.start_of_month(Date.current.year, merch_month: month)
+    end_date = MerchCalendar.end_of_month(Date.current.year, merch_month: month)
 
     # returns the month corresponding to each day in a given date range
     months_of_days = (start_date..end_date).map{ |date| date.strftime("%B") }
@@ -35,6 +46,13 @@ module DashboardHelper
     end
   end
 
+  def current_or_active_merch_year(params, merch_year)
+    if params[:merch_year].present?
+      return 'selected' if params[:merch_year] == merch_year.to_s
+    else
+      return 'selected' if merch_year == Date.current.year
+    end
+  end
 
   def start_of_period(params = {})
     if params[:merch_month].present?
@@ -53,4 +71,5 @@ module DashboardHelper
       MerchCalendar.end_of_month(Date.current.year, merch_month: month).strftime("%B %d, %Y")
     end
   end
+
 end
