e4f71819a3705e781d7d94bec2e1508168850b95
Alex Young <alex94945@gamil.com>
2017-06-06 22:30:31 -0700

    dynamically set amount and description

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,20 +1,31 @@
 class ChargesController < ApplicationController
+  before_action :amount_to_be_charged
+  before_action :set_description
+
   def new
   end
 
   def create
-    @amount = 500
-
     customer = StripeTool.create_customer(email: params[:stripeEmail],
                                           stripe_token: params[:stripeToken])
 
     charge = StripeTool.create_charge(customer_id: customer.id,
                                amount: @amount,
-                               description: 'Stripe Customer')
+                               description: @description)
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+
+  private
+
+    def amount_to_be_charged
+      @amount = 500
+    end
+
+    def set_description
+      @description = 'Style Scout Monthly Fee'
+    end
 
 end
