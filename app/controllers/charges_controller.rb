1d7c70cfd73b0672d948b76ff313c1d668b07130
Alex Young <alex94945@gamil.com>
2017-06-07 22:47:48 -0700

    finish stripe tutorial based integration

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,17 +2,23 @@ class ChargesController < ApplicationController
   before_action :amount_to_be_charged
   before_action :set_description
   before_action :authenticate_user!
+  before_action :set_plan
 
   def new
   end
 
   def create
-    customer = StripeTool.create_customer(email: params[:stripeEmail],
-                                          stripe_token: params[:stripeToken])
-
-    charge = StripeTool.create_charge(customer_id: customer.id,
-                               amount: @amount,
-                               description: @description)
+    if params[:subscription].include? 'yes'
+      StripeTool.create_membership(email: params[:stripeEmail],
+                                 stripe_token: params[:stripeToken],
+                                 plan: @plan)
+    else
+      customer = StripeTool.create_customer(email: params[:stripeEmail],
+                                            stripe_token: params[:stripeToken])
+      charge = StripeTool.create_charge(customer_id: customer.id,
+                                 amount: @amount,
+                                 description: @description)
+    end
 
     redirect_to thanks_path
 
@@ -22,6 +28,10 @@ class ChargesController < ApplicationController
   end
 
   private
+
+    def set_plan
+      @plan = 9999
+    end
 
     def amount_to_be_charged
       @amount = 500
--- app/models/concerns/stripe_tool.rb
+++ app/models/concerns/stripe_tool.rb
@@ -16,4 +16,12 @@ module StripeTool
     )
   end
 
+  def self.create_membership(email: email, stripe_token: stripe_token, plan: plan)
+    Stripe::Customer.create(
+      email: email,
+      source: stripe_token,
+      plan: plan
+    )
+  end
+
 end
