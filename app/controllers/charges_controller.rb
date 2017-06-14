9cf59878dcd434ee083dca0910d6b8164754f050
Alex Neigher <alex.neigher@snapdocs.com>
2017-06-13 22:22:34 -0600

    refactor into service obj

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,53 +1,25 @@
 class ChargesController < ApplicationController
   skip_before_action :check_payment_status
-  before_action :amount_to_be_charged
-  before_action :set_description
   before_action :authenticate_user!
-  before_action :set_plan
+  before_action :amount_in_cents
 
   def new
   end
 
   def create
+    service = StripeService.new(email: params[:stripeEmail], stripe_token: params[:stripeToken], user: current_user)
 
-    membership = StripeService.new().create_membership(email: params[:stripeEmail],
-                                         stripe_token: params[:stripeToken],
-                                         plan: @plan) #creates customer in StripeService
-
-    if membership
-      #looks like the membership creation was successful, persist the data locally
-      if current_user.payment_account.present?
-        payment_account = current_user.payment_account
-      else
-        payment_account = PaymentAccount.new
-      end
-
-      payment_account.update(user: current_user,
-                            stripe_customer_id: membership['id'],
-                            plan_id: set_plan,
-                            trial_period_active: true,
-                            status: :active)
+    if service.perform
+      redirect_to thanks_path
+    else
+      flash[:error] = service.errors
+      render :new
     end
-
-    redirect_to thanks_path
-
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
   end
 
   private
-
-    def set_plan
-      @plan = 9999
-    end
-
-    def amount_to_be_charged
+    def amount_in_cents
       @amount = 50 * 100 #cents
-    end
-
-    def set_description
-      @description = 'Style Scout Monthly Fee'
     end
 
     def thanks
--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -1,12 +1,44 @@
 class StripeService
 
-  def create_membership(email: email, stripe_token: stripe_token, plan: plan)
-    Stripe::Customer.create(
-      email: email,
-      source: stripe_token,
-      plan: plan
-    )
+  attr_accessor :errors
+
+  def initialize(email: email, stripe_token: stripe_token, user: user)
+    @plan = 9999
+    @email = email
+    @token = stripe_token
+    @user = user
+    @payment_account = @user.payment_account || PaymentAccount.new(user: user)
+    @errors = nil
+  end
+
+  def perform
+    create_membership
+    create_or_update_payment_account
   end
 
 
+  private
+    def create_membership
+      if @payment_account.stripe_customer_id.present?
+        @membership = Stripe::Customer.retrieve(@payment_account.stripe_customer_id)
+      else
+        @membership = Stripe::Customer.create(email: @email, source: @stripe_token, plan: @plan)
+      end
+
+      @membership['id'].present?
+    end
+
+    def create_or_update_payment_account
+      if @membership['id'].present?
+
+        @payment_account.update(
+                                stripe_customer_id: @membership['id'],
+                                plan_id: @plan,
+                                trial_period_active: true,
+                                status: :active
+                              )
+      else
+        @errors = 'there was a problem creating the customer'
+      end
+    end
 end
