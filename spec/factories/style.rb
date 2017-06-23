f36930a72fd33716b5cab2b1ee078e07b1db5ae7
Alex Young <alex94945@gamil.com>
2017-06-22 21:18:56 -0700

    add traits to style factory

--- spec/factories/style.rb
+++ spec/factories/style.rb
@@ -2,5 +2,18 @@ FactoryGirl.define do
   factory :style do
     vendor_style_number "123style-number"
     user
+
+    trait :created do
+      status "Created"
+    end
+
+    trait :pending do
+      status "Pending"
+    end
+
+    trait :placed do
+      status "Placed"
+    end
+
   end
 end
