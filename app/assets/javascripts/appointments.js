82689a66d2a211d4265f67e8954c248ceb3f053e
Alex Young <alex94945@gamil.com>
2018-03-18 21:48:01 -0700

    one more file rename to product set

--- app/assets/javascripts/appointments.js
+++
@@ -1,6 +0,0 @@
-
-$(document).ready(function(){
-  $('.product-set-container-toggle').click(function(){
-      $(this).find('.fa').toggleClass('fa-arrow-circle-down').toggleClass('fa-arrow-circle-up')
-  })
-})
