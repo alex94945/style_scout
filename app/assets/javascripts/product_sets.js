8c82d5ccb2e6259f52d24a969d893332a7abdcf2
Alex Young <alex94945@gamil.com>
2018-04-02 23:10:22 -0700

    last product set rename

---
+++ app/assets/javascripts/product_sets.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+  $('.product-set-container-toggle').click(function(){
+      $(this).find('.fa').toggleClass('fa-arrow-circle-down').toggleClass('fa-arrow-circle-up')
+  })
+})
